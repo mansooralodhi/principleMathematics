ee89930580ca61031cd7d7504e32729f7916e31f
mansooralodhi <malodhi1997@gmail.com>
2024-02-25 16:31:40 +0100

    done gradient descent with linar model

---
+++ src/algorithmic_differentiation/operator_overloading/gradient_descent.py
@@ -0,0 +1,37 @@
+import numpy as np
+from typing import List
+from src.algorithmic_differentiation.operator_overloading import tangent_mode
+
+# define a linear model:
+y = lambda x: 1.4 * x - 7
+
+# define a custom linear model
+f = lambda x, p1, p2: x * p1 + p2
+
+# define mean square error
+mse = lambda x, p1, p2: np.square(y(x) - f(x, p1, p2))
+
+# define learning rule
+def update_params(x: float, params: List[float]):
+    _x_grad, *params_grad = tangent_mode.gradient_vector(mse)([x, *params])
+    updated_params = np.asarray(params) - learning_rate * np.asarray(params_grad)
+    return list(updated_params)
+
+########## let's learn the right parameters for our custom linear model #########
+
+
+# initialize model parameters
+model_params = np.random.uniform(0, 1, (2,))
+print(f"Initial model parameters: {model_params}")
+
+# generate samples of input variable (x) and associated targets (y)
+n = 5000
+learning_rate = 0.01
+Xs = np.random.uniform(0, 1, (n,))
+Ys = np.asarray([y(i) for i in Xs])
+
+for i in range(n):
+    model_params = update_params(Xs[i], model_params)
+
+print(f"Final model parameters: {model_params}")
+print(f"Correct model parameters: [1.4, -7.0]")
--- src/algorithmic_differentiation/operator_overloading/tangent_mode.py
+++ src/algorithmic_differentiation/operator_overloading/tangent_mode.py
@@ -1,8 +1,12 @@
 from src.algorithmic_differentiation.operator_overloading.ops import *
-from src.algorithmic_differentiation.operator_overloading.verify_grad import *
+from src.algorithmic_differentiation.operator_overloading.verify_grad import froward_difference
 from src.algorithmic_differentiation.operator_overloading.dual_number import DualNumber
+from typing import Callable, Union, List, Literal
+from functools import wraps
 
 """
+Documentation
+-------------
 Forward-Mode or Tangent-Mode Algorithm
     1.  define function
     2.  initialize variables (primal values) 
@@ -15,7 +19,6 @@ DualNumbers:
     -   in forward-mode or tangent-mode, we can make tangent 1 of only
         one variable. in other words, we can compute derivative of
         all output functions w.r.t only single variable/input.
-
 """
 
 
@@ -26,12 +29,60 @@ DualNumbers:
 #             initialization or derivative w.r.t.
 #       2. find higher-order derivatives
 
-f = lambda x, y: x*y
+# todo:
+#       1.  a limitation of current implementation is that it only accepts
+#           f args as a flat list. In practise, the args can be any nested
+#           data structure.
+#           Future Goal:
+#               1.  write a code to flatten the input arguments. e.g. flatten_tree
+#               2.  write a code that produce a wrapper_function that operators
+#                   on flattened arguments. similar to what is given below:
+#                   from jax._src.api_util import flatten_fun_nokwargs
 
-x = DualNumber(10, 0)  # with respect to variable x
-y = DualNumber(20, 1)  # with respect to variable y
-z = DualNumber(5, 1)  # with respect to variable y
-print(f(x, y))
+def directional_derivative(f: Callable) -> Callable[[List[Literal[0, 1]], List[float]], float]:
+    @wraps(f)
+    def wrapper(wrt_arg: List[Literal[0, 1]], args: List[float]) -> float:
 
-ans = froward_difference(f, [10, 20], [0, 1])
-print(ans)
+        if len(wrt_arg) != len(args):
+            raise Exception("len(wrt_arg) != len(args) !")
+
+        dualNumbers = list()
+        for i in range(len(args)):
+            if wrt_arg[i] == 1:
+                dualNumbers.append(DualNumber(args[i], 1))
+            else:
+                dualNumbers.append(DualNumber(args[i], 0))
+        return f(*dualNumbers).tangent
+    return wrapper
+
+
+def gradient_vector(f: Callable) -> Callable[[List[float]], List[float]]:
+    @wraps(f)
+    def wrapper(args: List[float]):
+        gradVector = list()
+        for i in range(len(args)):
+            wrt_args = [1 if i == j else 0 for j in range(len(args))]
+            gradVector.append(directional_derivative(f)(wrt_args, args))
+        return gradVector
+    return wrapper
+
+
+def total_derivative(f: Callable) -> Callable[[List[float]], float]:
+    @wraps(f)
+    def wrapper(args: List[float]):
+        gradVector = gradient_vector(f)(args)
+        return sum(gradVector)
+    return wrapper
+
+
+if __name__ == "__main__":
+    func = lambda x, y: x * y
+    x = DualNumber(10, 0)
+    y = DualNumber(20, 1)  # with respect to variable y
+    print(func(x, y))
+    print(froward_difference(func, [10, 20], [0, 1]))
+    print(directional_derivative(func)([0, 1], [10, 20]))
+    print(directional_derivative(func)([1, 0], [10, 20]))
+    print(gradient_vector(func)([10, 20]))
+    print(directional_derivative(func)([1, 1], [10, 20]))
+    print(total_derivative(func)([10, 20]))
